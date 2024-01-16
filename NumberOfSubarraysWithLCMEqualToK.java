7dd62df23f6fe96daa362cd498ea5d494613f837
Aniket Singh <anikets101@gmail.com>
2024-01-16 12:01:09 +0530

    Array Problems

---
+++ NumberOfSubarraysWithLCMEqualToK.java
@@ -0,0 +1,62 @@
+/*
+    2470
+    Given an integer array nums and an integer k, return the number of subarrays of nums where the least common multiple of the subarray's elements is k.
+
+    A subarray is a contiguous non-empty sequence of elements within an array.
+
+    The least common multiple of an array is the smallest positive integer that is divisible by all the array elements.
+
+    
+
+Example 1:
+
+Input: nums = [3,6,2,7,1], k = 6
+Output: 4
+Explanation: The subarrays of nums where 6 is the least common multiple of all the subarray's elements are:
+- [3,6,2,7,1]
+- [3,6,2,7,1]
+- [3,6,2,7,1]
+- [3,6,2,7,1]
+Example 2:
+
+Input: nums = [3], k = 2
+Output: 0
+Explanation: There are no subarrays of nums where 2 is the least common multiple of all the subarray's elements.
+ */
+
+
+public class NumberOfSubarraysWithLCMEqualToK {
+    class Solution {
+        public int gcd(int a , int b){
+            if(a == 0){
+                return b;
+            }
+            return gcd(b%a,a);
+        }
+        public int lcm(int a , int b){
+            return (a*b)/gcd(a,b);
+        }
+        public int subarrayLCM(int[] nums, int k) {
+            
+            int result = 0;
+            for(int i=0; i< nums.length; i++){
+                int runningLCM = nums[i];
+                for(int j = i; j< nums.length; j++){
+                   runningLCM = lcm(runningLCM, nums[j]);
+                   if(runningLCM >k){
+                      break;
+                   }
+                   if( runningLCM == k) {
+                       result++;
+                   }
+    
+                }
+            }
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ RotateArray.java
@@ -0,0 +1,53 @@
+/*
+    189
+    Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+ */
+
+
+public class RotateArray {
+    class Solution {
+        public void reverse(int [] nums , int s, int e){
+            int temp;
+            while(s<e){
+                temp = nums[s];
+                nums[s] = nums[e];
+                nums[e] = temp;
+                s++;
+                e--;
+            }
+            
+        }
+        public void rotate(int[] nums, int k) {
+            
+            int n = nums.length;
+            k = k%n;
+            reverse(nums,0, n-1);
+            reverse(nums,0,k-1);
+            reverse(nums,k,n-1);
+    
+           
+            
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
