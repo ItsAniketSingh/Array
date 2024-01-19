a32670c4f458bc150e3c4339ac38cfb735ea6a29
Aniket Singh <95512260+ItsAniketSingh@users.noreply.github.com>
2024-01-19 12:10:37 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Array{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int [] arr = new int[5];
+    for(int i = 0 i < arr.length; i++){
+      System.out.print("Enter the Number for index : "+i+" : ");
+      arr[i] = sc.nextInt();
+    }
+    for(int i = 0; i < arr.length; i++){
+      System.out.println("The Value of index "+i+" is "+ arr[i]);
+    }
+  }
+}
