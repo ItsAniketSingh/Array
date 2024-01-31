4ed205665ea05a93c652ea49bab6f914bb834e6a
Aniket Singh <anikets101@gmail.com>
2024-01-31 09:10:51 +0530

    Array Problems

---
+++ EvaluateReversePolishNotation.java
@@ -0,0 +1,72 @@
+/**
+    150. Evaluate Reverse Polish Notation
+    You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+Evaluate the expression. Return an integer that represents the value of the expression.
+
+Note that:
+
+The valid operators are '+', '-', '*', and '/'.
+Each operand may be an integer or another expression.
+The division between two integers always truncates toward zero.
+There will not be any division by zero.
+The input represents a valid arithmetic expression in a reverse polish notation.
+The answer and all the intermediate calculations can be represented in a 32-bit integer.
+ 
+
+Example 1:
+
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+Example 2:
+
+Input: tokens = ["4","13","5","/","+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+Example 3:
+
+Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+Output: 22
+Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+ 
+*/
+public class EvaluateReversePolishNotation {
+
+    public int evalRPN(String[] tokens) {
+        int[] stack = new int[tokens.length];
+        int top = 0;
+        for(String s : tokens) {
+            char c = s.charAt(0);
+            if(c=='+') {
+                int b = stack[--top];
+                int a = stack[--top];
+                stack[top++] = a+b;
+            } else if(c=='-' && s.length()==1) {
+                int b = stack[--top];
+                int a = stack[--top];
+                stack[top++] = a-b;
+            } else if(c=='*') {
+                int b = stack[--top];
+                int a = stack[--top];
+                stack[top++] = a*b;
+            } else if(c=='/') {
+                int b = stack[--top];
+                int a = stack[--top];
+                stack[top++] = a/b;
+            } else 
+                stack[top++] = Integer.parseInt(s);
+        }
+        return stack[0];
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
