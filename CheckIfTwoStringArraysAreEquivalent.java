83c06d8a2784bf0b40249eff5fd7dbc868754b3b
Aniket Singh <anikets101@gmail.com>
2024-02-04 09:11:02 +0530

    Arrays Problems

--- CheckIfTwoStringArraysAreEquivalent.java
+++ CheckIfTwoStringArraysAreEquivalent.java
@@ -1,7 +1,6 @@
 /*
     1662
     Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
-
     A string is represented by an array if the array elements concatenated in order forms the string.
 
  
@@ -15,11 +14,12 @@ word1 represents string "ab" + "c" -> "abc"
 word2 represents string "a" + "bc" -> "abc"
 The strings are the same, so return true.
 
+*/
 
 
- */
 
- class Solution {
+public class CheckIfTwoStringArraysAreEquivalent {
+
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         
         String newWord1 = "";
@@ -32,10 +32,7 @@ The strings are the same, so return true.
         }
         return newWord1.equals(newWord2);
     }
-}
 
-
-public class CheckIfTwoStringArraysAreEquivalent {
     public static void main(String[] args) {
         
         
