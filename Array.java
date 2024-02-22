06c00726c4fde1933570ac642c5411e025b1b840
Aniket Singh <anikets101@gmail.com>
2024-02-22 19:56:37 +0530

    Array Problems

--- Array.java
+++ Array.java
@@ -1,9 +1,17 @@
 import java.util.*;
 
 public class Array{
+
+
+  public static void create(int size){
+    int [] arr = new int[size];
+  }
+
+  
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     int [] arr = new int[5];
+
     for(int i = 0; i < arr.length; i++){
       System.out.print("Enter the Number for index : "+i+" : ");
       arr[i] = sc.nextInt();
