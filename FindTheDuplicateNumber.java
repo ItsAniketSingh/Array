2cbbd015ff6f3f9d1aff4ca2fd9d9235012a3784
Aniket Singh <anikets101@gmail.com>
2024-02-27 21:02:29 +0530

    Array Problems

---
+++ FindTheDuplicateNumber.java
@@ -0,0 +1,37 @@
+/*
+    287. Find the Duplicate Number
+    Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+    There is only one repeated number in nums, return this repeated number.
+    You must solve the problem without modifying the array nums and uses only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+ 
+ */
+
+import java.util.Arrays;
+
+public class FindTheDuplicateNumber {
+
+     public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] == nums[i + 1]) {
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
