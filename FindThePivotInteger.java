b0bf067fb972526e8fc94e98e9b1ee94c7618272
Aniket Singh <anikets101@gmail.com>
2024-03-13 07:23:51 +0530

    Array Problems

---
+++ FindThePivotInteger.java
@@ -0,0 +1,54 @@
+/*  
+    2485. Find the Pivot Integer
+    Given a positive integer n, find the pivot integer x such that:
+    The sum of all elements between 1 and x inclusively equals the sum of all elements between x and n inclusively.
+    Return the pivot integer x. If no such integer exists, return -1. It is guaranteed that there will be at most one pivot index for the given input.
+
+    
+
+Example 1:
+Input: n = 8
+Output: 6
+Explanation: 6 is the pivot integer since: 1 + 2 + 3 + 4 + 5 + 6 = 6 + 7 + 8 = 21.
+
+Example 2:
+Input: n = 1
+Output: 1
+Explanation: 1 is the pivot integer since: 1 = 1.
+
+Example 3:  
+Input: n = 4
+Output: -1
+Explanation: It can be proved that no such integer exist.
+ */
+
+public class FindThePivotInteger {
+
+
+    public int pivotInteger(int n) {
+        int leftSum = 0;
+        int rightSum = 0;
+        int left = 1;
+        int right = n;
+        while(left<=right){
+            if(left == right && leftSum == rightSum){
+                return left;
+            }
+            if(leftSum < rightSum){
+                leftSum += left;
+                left++;
+            }
+            else{
+                rightSum += right;
+                right--;
+            }
+        }
+        return -1;
+        
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
